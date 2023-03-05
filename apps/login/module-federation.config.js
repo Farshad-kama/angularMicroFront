a68037124fdf097ca236d4471d6e914c8810f12a
Farshad <farhadkama@outlook.com>
2023-03-05 15:35:47 +0330

    bug fixed

--- apps/login/module-federation.config.js
+++ apps/login/module-federation.config.js
@@ -2,12 +2,5 @@ module.exports = {
   name: 'login',
   exposes: {
     './Module': 'apps/login/src/app/remote-entry/entry.module.ts',
-  },
-  devServer: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
   }
 };
